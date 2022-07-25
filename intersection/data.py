c826117c77769bb1cb5f4215469c0ed0e801d872
gruvw <gruvw.dev@gmail.com>
2022-07-25 22:03:36 +0200

    Changed Data modeling

--- intersection/data.py
+++ intersection/data.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from uuid import uuid4
 
 
@@ -5,53 +6,53 @@ class IntersectionData:
 
     def __init__(self, max_match_delay):
         self._users = {}
-        self._games = {}
+        # self._games = {}
         self._max_match_delay = max_match_delay
-        self._waitingUser = None
+        self._waiting_room = Game(uuid4())
 
-    def _user_in_queue(self):
-        return self._waitingUser if self._waitingUser and self._waitingUser.was_registered_before(self._max_match_delay) else None
-
-    def _register_user(self, user_id, user_name):
-        if user_id in self._users and self._users[user_id]._game:
-            self._games.pop(self._users[user_id]._game._name)
-            self._users[user_id]._game.terminate()
-        self._users[user_id] = User(user_id, user_name)
+    def get_user(self, user_id):
+        if user_id not in self._users:
+            return None
         return self._users[user_id]
 
-    def _register_anonymous_user(self, user):
-        user_in_queue = self._user_in_queue()
-        if user_in_queue:
-            game = Game((user, user_in_queue), uuid4())
-            self._games[game._name] = game
-            return True
-        self._waitingUser = user
-        return False
+    def create_user(self, user_id, username, chat_id):
+        user = User(user_id, username, chat_id)
+        self._users[user_id] = user
+        return user
 
-    def _register_named_user(self, user, name):
-        if name in self._games:
-            return self._games[name]._add_player(user)
-        game = Game((user), name)
-        self._games[game._name] = game
-        return True
+    def get_or_create_game(self, game_name):
+        if not game_name:
+            # TODO Waiting room max delay
+            return self._waiting_room
 
-    def register_user(self, user_id, user_name, name=None):
-        user = self._register_user(user_id, user_name)
-        if name:
-            return self._register_named_user(user, name)
-        return self._register_anonymous_user(user)
+        for user in self._users.values():
+            if user.game and user.game.name == game_name:
+                return user.game
+        game = Game(game_name)
+        return game
+
+    def join_game(self, user, game: Game):
+        if game.is_full():
+            raise Exception(f"Trying to join a game that is already full! {user}, {game}")
+
+        user.game = game
+        game._players.append(user)
+
+        game.reset()
+
+        if game == self._waiting_room and game.is_full():
+            self._waiting_room = Game(uuid4())
 
 
 class User:
 
-    def __init__(self, user_id, user_name):
+    def __init__(self, user_id, user_name, chat_id):
         self._registered_time = 0
         self._user_id = user_id
-        self._user_name = user_name
-        self._game = None
-
-    def _set_game(self, game):
-        self._game = game
+        self.user_name = user_name
+        self.chat_id = chat_id
+        self.game: Game = None
+        self.current_word = ""
 
     def was_registered_before(time):
         return True # TODO
@@ -59,21 +60,39 @@ class User:
 
 class Game:
 
-    def __init__(self, players, name):
-        self._name = name
-        self._player_ids = [p._user_id for p in players]
-        for p in players:
-            p._set_game(self)
+    def __init__(self, name):
+        self.name = name
+        self._players: list[User] = []
+        self.rounds_count = 0
 
-    def _add_player(self, player):
-        if len(self._player_ids) >= 2:
-            return False
-        self._player_ids.append(player._user_id)
-        player._set_game(self)
-        return True
+    def get_players_except(self, excepted_player):
+        # TODO convert in get opponent
+        return (p for p in self._players if p != excepted_player)
+
+    def get_chat_ids(self):
+        return (p.chat_id for p in self._players)
+
+    def get_broadcast_against(self):
+        return ((p.chat_id, self.get_players_except(p)) for p in self._players)
+
+    def is_full(self):
+        return len(self._players) >= 2
+
+    def is_empty(self):
+        return len(self._players) == 0
+
+    def new_round(self):
+        for player in self._players:
+            player.current_word = ""
+        self.rounds_count += 1
+
+    def reset(self):
+        for player in self._players:
+            player.current_word = ""
+        self.rounds_count = 0
 
     def terminate(self):
-        pass # TODO
-
-
-# TODO add messages feedback
+        for player in self._players:
+            player.game = None
+            player.current_word = ""
+        self._players.clear()
--- intersection/telegram_bot.py
+++ intersection/telegram_bot.py
@@ -1,18 +1,21 @@
 import os
 from dotenv import load_dotenv
-from telegram import ParseMode, InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram import ParseMode, InputTextMessageContent, Update, InlineQueryResultArticle, InputMessageContent
 from telegram.utils.helpers import escape_markdown
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler, CallbackContext, ChosenInlineResultHandler
 
-from .data import IntersectionData
+from .data import IntersectionData, User
 
 
 load_dotenv()
 updater = Updater(token=os.getenv("TELEGRAM_BOT_TOKEN"), use_context=True)
 dispatcher = updater.dispatcher
+bot_username = dispatcher.bot.name
 
-gameData = IntersectionData()
+gameData = IntersectionData(0)
 
+
+# Decorators
 
 def commandHandler(func):
     handler = CommandHandler(func.__name__, func)
@@ -25,9 +28,16 @@ def inlineQueryHandler(func):
     return func
 
 
+def chosenInlineResultHandler(func):
+    dispatcher.add_handler(ChosenInlineResultHandler(func))
+    return func
+
+
+# Telegram
+
 @commandHandler
-def start(update, context):
-    update.message.reply_text('Hi!')
+def start(update, context: CallbackContext):
+    update.message.reply_text("Hello and welcome to The Intersection Game!\nThis is a 2 player game so you must find another player to start a game.\nTo do so you must use the /play command. Typing /play will put you inside a game, if another player also uses /play soon after you, the game will start.\nIf you specify a password after /play you will only play against a player that used the same password.\nHave Fun !")
 
 
 @commandHandler
@@ -37,40 +47,80 @@ def help(update, context):
 
 
 @commandHandler
-def play(update, context):
-    if not context.args:
-        pass
-    update.message.reply_text('coucou')
+def play(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.name
+    chat_id = update.effective_chat.id
+
+    game_name = context.args[0] if context.args else None
+
+    user = gameData.get_user(user_id) or gameData.create_user(user_id, user_name, chat_id)
+
+    if user.game:
+        for chat_id in user.game.get_chat_ids():
+            context.bot.send_message(chat_id, "Your game was terminated.")
+        user.game.terminate()
+
+    game = gameData.get_or_create_game(game_name)
+
+    if game.is_full():
+        context.bot.send_message(user.chat_id, "This game is already full!")
+        return
+
+    gameData.join_game(user, game)
+    context.bot.send_message(user.chat_id, f"You entered the game {game.name}.")
+
+    if game.is_full():
+        for chat_id, opponents in game.get_broadcast_against():
+            context.bot.send_message(chat_id, f"Your game is ready to start!\nYou are playing against: {','.join(o.user_name for o in opponents)}.")
+        return
+
+    context.bot.send_message(user.chat_id, f"Waiting for another player to join the game...")
 
 
 @inlineQueryHandler
-def inlineQuery(update, context):
+def inlineQuery(update: Update, context):
     query = update.inline_query.query
+
+    user = gameData.get_user(update.inline_query.from_user.id)
+    game = user.game if user else None
+
+    if not game:
+        update.inline_query.answer([], switch_pm_text="Play !", switch_pm_parameter="_")
+        return
+
+    if not game.is_full():
+        results = [
+            InlineQueryResultArticle(
+                id="0",
+                title="Invite player",
+                description="Two players are required to start the game.",
+                input_message_content=InputTextMessageContent(f"Play Intersection with me using {bot_username}."),
+            )
+        ]
+        update.inline_query.answer(results)
+        return
 
     if query == "":
         return
 
-    results = [
-        InlineQueryResultArticle(
-            id="1",
-            title="Caps",
-            input_message_content=InputTextMessageContent(query.upper()),
-        ),
-        InlineQueryResultArticle(
-            id="2",
-            title="Bold",
-            input_message_content=InputTextMessageContent(
-                f"*{escape_markdown(query)}*", parse_mode=ParseMode.MARKDOWN
-            ),
-        ),
-        InlineQueryResultArticle(
-            id="3",
-            title="Italic",
-            input_message_content=InputTextMessageContent(
-                f"_{escape_markdown(query)}_", parse_mode=ParseMode.MARKDOWN
-            ),
-        ),
-    ]
+    other_player = game.get_players_except(user)[0]
+    if other_player.current_word:
+        result_message = f"You won 🎉! It took you {game.rounds_count} rounds to settle.\nTalk to {bot_username} to play again." if other_player.current_word == query else "Try again!"
+        results = [
+            InlineQueryResultArticle(
+                id="2",
+                title="Submit Your Word",
+                input_message_content=InputTextMessageContent(f"Summary:\n1. {other_player.user_name} -> {other_player.current_word}\n2. {user.user_name} -> {query}\n\n{result_message}"),
+            )
+        ]
+        update.inline_query.answer(results)
+        return
 
+    # TODO id 1
     update.inline_query.answer(results)
 
+
+@chosenInlineResultHandler
+def inlineResult(update: Update, context: CallbackContext):
+    pass
